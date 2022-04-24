1744defe3bc41c47581b3d6a864b37385cfbc0eb
Carlospq17 <carlospq_17@hotmail.com>
2022-04-24 18:13:59 -0500

    Implement CRUD requests structure

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -7,4 +7,23 @@ use Illuminate\Http\Request;
 class AlumnosController extends Controller
 {
     //
+    public function getAll(){
+        return "Get All";
+    }
+
+    public function getAlumno($id){
+        return "Get Alumno ".$id;
+    }
+
+    public function createAlumno(){
+        return redirect()->back();
+    }
+
+    public function updateAlumno($id){
+        return "Update Alumno ".$id;
+    }
+
+    public function deleteAlumno($id){
+        return "Delete Alumno ".$id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlumnosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/alumnos', [AlumnosController::class, 'getAll']);
+
+Route::get('/alumnos/{id}', [AlumnosController::class, 'getAlumno']);
+
+Route::post('/alumnos', [AlumnosController::class, 'createAlumno']);
+
+Route::put('/alumnos/{id}', [AlumnosController::class, 'updateAlumno']);
+
+Route::delete('/alumnos/{id}', [AlumnosController::class, 'deleteAlumno']);
